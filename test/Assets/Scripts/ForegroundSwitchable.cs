67f7e7999c286337260b4c16da6f2fde900d8f8e
Adrian Miller <adrianrichmiller@gmail.com>
2019-02-26 21:25:39 -0800

    Added Orb Grab and Tiles becoming existant/nonexistant

---
+++ test/Assets/Scripts/ForegroundSwitchable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForegroundSwitchable : MonoBehaviour {
+	//public bool isVisible = false;
+	public GameController gameController;
+	private BoxCollider2D outline;
+	private SpriteRenderer sirRender;
+	public float s_red = 255.0f, s_green = 255.0f, s_blue = 255.0f; 
+	Color s_combined;
+	// Use this for initialization
+	void Start (){
+		outline = GetComponent<BoxCollider2D> ();
+		sirRender = GetComponent<SpriteRenderer> ();
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+		s_combined = new Color (s_red, s_green, s_blue);
+		outline.enabled = gameController.isVisible;
+		if (gameController.isVisible) {
+			sirRender.color = s_combined;
+		}
+
+	}
+}
---
+++ test/Assets/Scripts/Orb_Get.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb_Get : MonoBehaviour {
+	public Object deleteThis;
+	private CircleCollider2D orb;
+	public GameController gameController;
+	// Use this for initialization
+	void Start () {
+		orb = GetComponent<CircleCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 10, 0);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("Touched Sphere");
+		gameController.isVisible = true;
+		Destroy (deleteThis);
+
+	}
+}
