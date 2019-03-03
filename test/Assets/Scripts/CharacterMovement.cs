a72920cec469223ee5b48ed58f155c2e7c978893
Cett <tolvecspellcaster@gmail.com>
2019-03-03 14:08:54 -0800

    Added Jumoing to character controls, Travis, 3/3/19

--- test/Assets/Scripts/CharacterMovement.cs
+++ test/Assets/Scripts/CharacterMovement.cs
@@ -9,8 +9,15 @@ public class CharacterMovement : MonoBehaviour {
 	Vector3 currentPosition;
 	GameObject lastCheckpoint;
 
+	//Jumping variables
+	public bool jumping;
+	public float jumpForce = 100.0f;
+
+
 	float hSpeed;
 	public float walkSpeed = 10f;
+
+
 	// Use this for initialization
 	void Start () {
 		player = GetComponent<Transform> ();
@@ -25,7 +32,7 @@ public class CharacterMovement : MonoBehaviour {
 	void Update () {
 		hSpeed = Input.GetAxis ("Horizontal") * walkSpeed;
 
-		playersRigidbody.AddForce (Vector2.right*hSpeed);
+		playersRigidbody.AddForce (Vector2.right * hSpeed);
 
 		if (facingLeft && (hSpeed > 0)) {
 			Flip ();
@@ -34,8 +41,16 @@ public class CharacterMovement : MonoBehaviour {
 		if (!facingLeft && (hSpeed < 0)) {
 			Flip ();
 		}
-
+	
+		//jumping controls, Travis
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.Space)) {
+				if (!jumping) {
+					playersRigidbody.AddForce (Vector2.up * jumpForce);
+					jumping = true;	
+				}	
+		}
 	}
+
 
 	public void updateCheckpoint(GameObject other) {
 		lastCheckpoint = other;
