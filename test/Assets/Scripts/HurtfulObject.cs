99e3bf55bd3b1b49459c7b1fc2e4a664ccc5150c
Ryan E <raenglandwork@gmail.com>
2019-03-27 14:42:02 -0700

    Added HurtfulObject script, because the other way doesn't work with the new player physics.

---
+++ test/Assets/Scripts/HurtfulObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtfulObject : MonoBehaviour {
+
+	public Vector3 destination;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawLine (transform.position, destination);
+	}
+}
