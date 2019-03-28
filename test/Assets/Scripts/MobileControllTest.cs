8c45df0ca4e69eba67b66a44edc75f7a29ff981e
Adrian Miller <adrianrichmiller@gmail.com>
2019-03-27 22:55:43 -0700

    added basic mobile controlles

--- test/Assets/Scripts/MobileControllTest.cs
+++ test/Assets/Scripts/MobileControllTest.cs
@@ -1,45 +1,35 @@
-﻿using UnityEngine;
-
-[RequireComponent(typeof(Player))]
-public class PlayerInput : MonoBehaviour
-{
-    private Player player;
-
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MobileControllTest : MonoBehaviour {
 	bool leftSideTouched = false;
 	bool rightSideTouched = false;
 	Rect leftSide,rightSide;
 
-    private void Start()
-    {
-        player = GetComponent<Player>();
+	public Text responeseText;
 
+	SpriteRenderer sRend;
+	// Use this for initialization
+	void Start () {
+		sRend = GetComponent<SpriteRenderer> ();
+		responeseText.text = "";
 		leftSide = new  Rect(0, 0, Screen.width / 4.0f, Screen.height);
 		rightSide = new Rect(((Screen.width/4f)*3),0,Screen.width / 4.0f,Screen.height);
+	}
 
-    }
-
-    private void Update()
-    {
-        Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        player.SetDirectionalInput(directionalInput);
-
-        if (Input.GetButtonDown("Vertical"))
-        {
-            player.OnJumpInputDown();
-        }
-
-        if (Input.GetButtonUp("Vertical"))
-        {
-            player.OnJumpInputUp();
-        }
-
+	// Update is called once per frame
+	void Update () {
 		mobileControlls ();
-    }
+	}
 
 	private void mobileControlls(){
 		if (Input.touchCount > 0) {
+			responeseText.text = "Touch Encountered";
+			sRend.color = Color.black;
 			Vector2[] touchPlaces = new Vector2[Input.touches.Length];
-			for (int i = 0; i < touchPlaces.Length; i++) {
+			for (int i = 0; i < touchPlaces.Length; i++){
+				responeseText.text = touchPlaces [i].ToString();
 				touchPlaces [i] = Input.touches [i].position;
 			}
 
@@ -47,25 +37,24 @@ public class PlayerInput : MonoBehaviour
 				if (leftSide.Contains (touchPlaces [j])) {
 					leftSideTouched = true;
 				}
-				
+
 				if (rightSide.Contains (touchPlaces [j])) {
 					rightSideTouched = true;
 				}
 			}
 
 			if (rightSideTouched && !leftSideTouched) {
-				player.SetDirectionalInput (Vector2.right);
+				sRend.color = Color.magenta;
 			}
 
 			if (!rightSideTouched && leftSideTouched) {
-				player.SetDirectionalInput (Vector2.left);
+				sRend.color = Color.cyan;
 			}
 
 			if (rightSideTouched && leftSideTouched) {
-				player.OnJumpInputDown();
+				sRend.color = Color.green;
 			}
 		}
-		//player.OnJumpInputUp();
 		leftSideTouched = false;
 		rightSideTouched = false;
 	}
