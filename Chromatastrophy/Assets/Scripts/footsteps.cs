f996ca2942038a5e4b0668605f0186445a50c8cf
Jordan <forastic@gmail.com>
2019-04-17 16:55:25 -0700

    added footstep sounds and script to play while walking

---
+++ Chromatastrophy/Assets/Scripts/footsteps.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class footsteps : MonoBehaviour {
+
+	[SerializeField]
+	private AudioClip[] clips;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	private void Awake () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	public void step(){
+		AudioClip clip = GetRandomClip ();
+		audioSource.PlayOneShot(clip);
+
+	}
+
+	private AudioClip GetRandomClip(){
+		return clips[UnityEngine.Random.Range(0, clips.Length)];
+	}
+
+}
