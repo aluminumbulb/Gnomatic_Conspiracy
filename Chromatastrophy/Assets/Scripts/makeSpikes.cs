f08c589a3096a14fde3f9263f689eed61bbffdb0
Ryan E <raenglandwork@gmail.com>
2019-05-06 17:32:50 -0700

    Updated castle.

--- Chromatastrophy/Assets/Scripts/makeSpikes.cs
+++ Chromatastrophy/Assets/Scripts/makeSpikes.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class makeSpikes : MonoBehaviour {
 
-	public GameController _gameController;
+	// public GameController _gameController;
 
 	// Use this for initialization
 	void Start () {
